3a351195fe29de9bf1ea949b25843fd187cfb0ab
amirhosseindehghani <amir.dehghani.official@gmail.com>
2020-11-20 12:34:09 +0330

    v-1.0.2

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-module.exports.E = (scoreA , scoreB) => 1/(1+Math.pow(10,(scoreA - scoreB)/400))
+module.exports = class Rating {
+  constructor() {
+  }
+
+  resolveE(scoreA , scoreB) {
+    return 1/(1+Math.pow(10,(scoreA - scoreB)/400))
+  }
+};
